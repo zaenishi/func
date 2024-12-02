baf659a7a0127a6a4dc478cd4020c61ab2f3a7f8
zaenishi <zaenishi@gmail.com>
2024-12-02 20:47:55 +0700

    First

---
+++ services.js
@@ -0,0 +1,6 @@
+async function conn(db, func, color, util) {
+console.log(color.red('Script ini dibuat oleh zaenishi 🍄'))
+console.log(color.white(`Website: zaenishi.xyz\nGithub: zaenishi\nInstagram: zaenishi\nKontak: 62831-8822-9366`))
+}
+
+module.exports = { conn }
