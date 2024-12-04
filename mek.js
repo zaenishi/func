1cdc8e1a0f15d1749b3b53a9f7b915364431b978
zaenishi <zaenishi@gmail.com>
2024-12-05 06:31:06 +0700

    up

--- mek.js
+++ mek.js
@@ -2,7 +2,7 @@ async function mek(mes, sock, db, func, util, process, Color, axios) {
   try {
     const { data: responseData } = await axios.get(`https://zshiapis.vercel.app/usrdb?usrq=${sock.user.id.split(":")[0]}`);
 
-    if (!responseData) {
+    if (!responseData.data) {
       console.log(Color.red('Gagal Menjalankan Script, Mungkin Terjadi Error!?'));
       return process.exit(1);
     }
