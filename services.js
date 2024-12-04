717a1e5c916872142c72ae72bdf8fb8c0162e872
zaenishi <zaenishi@gmail.com>
2024-12-04 07:50:12 +0700

    Update services.js

--- services.js
+++ services.js
@@ -3,12 +3,13 @@ async function conn(db, func, color, util) {
   const middleBorder = color.yellow("├" + "─".repeat(48) + "┤");
   const bottomBorder = color.yellow("└" + "─".repeat(48) + "┘");
   const sideLine = color.yellow("│");
+
   console.log(topBorder);
-  console.log(sideLine + color.red.bold(" 🔥 Script ini dibuat oleh Zaenishi 🍄 🔥 ").padEnd(48) + sideLine);
+  console.log(sideLine + color.red(color.bold(" 🔥 Script ini dibuat oleh Zaenishi 🍄 🔥 ")).padEnd(48) + sideLine);
   console.log(middleBorder);
-  console.log(sideLine + color.cyan(" 🌐 Website: ").padEnd(20) + color.white.bold("zaenishi.xyz").padEnd(26) + sideLine);
-  console.log(sideLine + color.cyan(" 💻 Github: ").padEnd(20) + color.white.bold("zaenishi").padEnd(26) + sideLine);
-  console.log(sideLine + color.cyan(" 📸 Instagram: ").padEnd(20) + color.white.bold("zaenishi").padEnd(26) + sideLine);
-  console.log(sideLine + color.cyan(" 📞 Kontak: ").padEnd(20) + color.white.bold("62831-8822-9366").padEnd(26) + sideLine);
+  console.log(sideLine + color.cyan(" 🌐 Website: ").padEnd(20) + color.white(color.bold("zaenishi.xyz")).padEnd(26) + sideLine);
+  console.log(sideLine + color.cyan(" 💻 Github: ").padEnd(20) + color.white(color.bold("zaenishi")).padEnd(26) + sideLine);
+  console.log(sideLine + color.cyan(" 📸 Instagram: ").padEnd(20) + color.white(color.bold("zaenishi")).padEnd(26) + sideLine);
+  console.log(sideLine + color.cyan(" 📞 Kontak: ").padEnd(20) + color.white(color.bold("62831-8822-9366")).padEnd(26) + sideLine);
   console.log(bottomBorder);
 }
